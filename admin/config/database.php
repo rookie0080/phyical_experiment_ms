30469a86b2f7bc753aaeeeac53e9302b5ee119d5
rookie0080 <rookie0080@163.com>
2021-03-01 21:24:17 +0800

    修改数据库连接信息

--- admin/config/database.php
+++ admin/config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('HOSTNAME', '120.76.124.44');
+	define('HOSTNAME', 'localhost');	// 维护记录：localhost，不是ip地址
 	define('HOSTUSER','root');
 	define('HOSTPWD','zzs11130COMEON');
 	define('HOSTDB','physics_manage');
